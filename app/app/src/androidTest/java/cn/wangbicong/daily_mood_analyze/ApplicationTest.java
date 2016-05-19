975bc97b04815ebc1ada12783feb05a76a8796e2
wangbicong <wangbicong@hotmail.com>
2016-05-19 20:04:28 +0800

    app init

---
+++ app/app/src/androidTest/java/cn/wangbicong/daily_mood_analyze/ApplicationTest.java
@@ -0,0 +1,13 @@
+package cn.wangbicong.daily_mood_analyze;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
